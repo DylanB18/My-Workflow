145033a53525f87a6eebf1cd8d885b34a3d38a23
Dylan Bober <deappmaker@gmail.com>
2019-06-21 23:48:21 -0400

    Added app install script

---
+++ app-install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh 
+
+# Hey there! You may need to give me permission to run with "chmod +x /path/to/file"
+
+echo "This script requires Homebrew for Mac"
+echo "If you get errors, install Homebrew by going to brew.sh"
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install discord
+brew cask install minecraft
+brew cask install lastpass
+brew cask install slack
+brew cask install skype
+brew cask install steam
+brew cask install whatsapp
+brew cask install houseparty
+brew cask install signal
+brew cask install flux
+brew cask install karabiner-elements
+brew cask install dropbox
+brew cask install private-internet-access
+brew cask install onedrive
+brew cask install gitkraken
+brew cask install oversight
